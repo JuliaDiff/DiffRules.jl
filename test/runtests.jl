c1c4b68d91737bc41eea4d1826ea9399d0cc9903
Jarrett Revels <jarrettrevels@gmail.com>
2017-08-23 20:28:37 -0400

    add namespace support

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Base.Test
-using RealInterface
 using SpecialFunctions
 using DiffRules
 
@@ -10,20 +9,18 @@ function finitediff(f, x)
     return (f(x + ϵ) - f(x - ϵ)) / (ϵ + ϵ)
 end
 
-for f in vcat(RealInterface.UNARY_ARITHMETIC, RealInterface.UNARY_MATH, RealInterface.UNARY_SPECIAL_MATH)
-    if DiffRules.hasdiffrule(f, 1)
-        deriv = DiffRules.diffrule(f, :x)
+for (M, f, arity) in DiffRules.diffrules()
+    if arity == 1
+        @test DiffRules.hasdiffrule(M, f, 1)
+        deriv = DiffRules.diffrule(M, f, :x)
         modifier = in(f, (:asec, :acsc, :asecd, :acscd, :acosh, :acoth)) ? 1 : 0
         @eval begin
             x = rand() + $modifier
             @test isapprox($deriv, finitediff($f, x), rtol=0.05)
         end
-    end
-end
-
-for f in vcat(RealInterface.BINARY_MATH, RealInterface.BINARY_ARITHMETIC, RealInterface.BINARY_SPECIAL_MATH)
-    if DiffRules.hasdiffrule(f, 2)
-        derivs = DiffRules.diffrule(f, :x, :y)
+    elseif arity == 2
+        @test DiffRules.hasdiffrule(M, f, 2)
+        derivs = DiffRules.diffrule(M, f, :x, :y)
         @eval begin
             x, y = rand(1:10), rand()
             dx, dy = $(derivs[1]), $(derivs[2])
