b7464969732b9fb3682fe348aed80481e2ba756a
Jarrett Revels <jarrettrevels@gmail.com>
2018-06-25 14:32:51 -0400

    tmp

--- src/api.jl
+++ src/api.jl
@@ -24,27 +24,29 @@ Examples:
 
 """
 macro define_diffrule(def)
-    @assert isa(def, Expr) && def.head == :(=) "Diff rule expression does not have a left and right side"
+    return define_diffrule(__module__, def)
+end
+
+function define_diffrule(m::Module, def::Expr)
+    @assert(Meta.isexpr(def, :(=)) && Meta.isexpr(def.args[1], :call),
+            "diffrule must be written in method definition assignment form")
     lhs = def.args[1]
     rhs = def.args[2]
-    @assert isa(lhs, Expr) && lhs.head == :call "LHS is not a function call"
     qualified_f = lhs.args[1]
-    @assert isa(qualified_f, Expr) && qualified_f.head == :(.) "Function is not qualified by module"
+    @assert(isa(qualified_f, Expr) && qualified_f.head == :(.),
+            "rule name must be fully qualified (e.g. `Base.sin`, not `sin`)")
     M = qualified_f.args[1]
     f = _get_quoted_symbol(qualified_f.args[2])
     args = lhs.args[2:end]
-    rule = Expr(:->, Expr(:tuple, args...), rhs)
-    key = Expr(:tuple, Expr(:quote, M), Expr(:quote, f), length(args))
-    return esc(quote
-        $DiffRules.DEFINED_DIFFRULES[$key] = $rule
-        $key
-    end)
+    key = (M, f, length(args))
+    DiffRules.DEFINED_DIFFRULES[key] = Core.eval(m, Expr(:->, Expr(:tuple, args...), rhs))
+    return key
 end
 
 """
     diffrule(M::Union{Expr,Symbol}, f::Symbol, args...)
 
-Return the derivative expression for `M.f` at the given argument(s), with the argument(s)
+Return the derivative expression(s) for `M.f` at the given argument(s), with the argument(s)
 interpolated into the returned expression.
 
 In the `n`-ary case, an `n`-tuple of expressions will be returned where the `i`th expression
@@ -53,18 +55,18 @@ is the derivative of `f` w.r.t the `i`th argument.
 Examples:
 
     julia> DiffResults.diffrule(:Base, :sin, 1)
-    :(cos(1))
+    (:(cos(1)),)
 
     julia> DiffResults.diffrule(:Base, :sin, :x)
-    :(cos(x))
+    (:(cos(x)),)
 
     julia> DiffResults.diffrule(:Base, :sin, :(x * y^2))
-    :(cos(x * y ^ 2))
+    (:(cos(x * y ^ 2)),)
 
     julia> DiffResults.diffrule(:Base, :^, :(x + 2), :c)
     (:(c * (x + 2) ^ (c - 1)), :((x + 2) ^ c * log(x + 2)))
 """
-diffrule(M::Union{Expr,Symbol}, f::Symbol, args...) = DEFINED_DIFFRULES[M,f,length(args)](args...)
+diffrule(M::Union{Expr,Symbol}, f::Symbol, args...) = Base.invokelatest(DEFINED_DIFFRULES[M,f,length(args)], args...)
 
 """
     hasdiffrule(M::Union{Expr,Symbol}, f::Symbol, arity::Int)
