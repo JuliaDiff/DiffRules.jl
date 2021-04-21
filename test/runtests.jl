3f71077f976bcc5ee23a4560e06b621dcf512e3d
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-04-21 06:31:55 +0200

    added tests for different types

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,32 +18,38 @@ end
 
 non_numeric_arg_functions = [(:Base, :rem2pi, 2)]
 
-for (M, f, arity) in DiffRules.diffrules()
-    (M, f, arity) ∈ non_numeric_arg_functions && continue
-    if arity == 1
-        @test DiffRules.hasdiffrule(M, f, 1)
-        deriv = DiffRules.diffrule(M, f, :goo)
-        modifier = in(f, (:asec, :acsc, :asecd, :acscd, :acosh, :acoth)) ? 1 : 0
-        @eval begin
-            goo = rand() + $modifier
-            @test isapprox($deriv, finitediff($M.$f, goo), rtol=0.05)
-            # test for 2pi functions
-            if "mod2pi" == string($M.$f)
-                goo = 4pi + $modifier
-                @test NaN === $deriv
-            end
-        end
-    elseif arity == 2
-        @test DiffRules.hasdiffrule(M, f, 2)
-        derivs = DiffRules.diffrule(M, f, :foo, :bar)
-        @eval begin
-            foo, bar = rand(1:10), rand()
-            dx, dy = $(derivs[1]), $(derivs[2])
-            if !(isnan(dx))
-                @test isapprox(dx, finitediff(z -> $M.$f(z, bar), float(foo)), rtol=0.05)
-            end
-            if !(isnan(dy))
-                @test isapprox(dy, finitediff(z -> $M.$f(foo, z), bar), rtol=0.05)
+for T in [Float32, Float64]
+    for (M, f, arity) in DiffRules.diffrules()
+        @testset "$M.$f $(arity)" begin
+            (M, f, arity) ∈ non_numeric_arg_functions && continue
+            if arity == 1
+                @test DiffRules.hasdiffrule(M, f, 1)
+                deriv = DiffRules.diffrule(M, f, :goo)
+                modifier = in(f, (:asec, :acsc, :asecd, :acscd, :acosh, :acoth)) ? 1 : 0
+                @eval begin
+                    goo = $T(rand() + $modifier)
+                    @test $deriv isa $T
+                    @test isapprox($deriv, finitediff($M.$f, goo), rtol=0.05)
+                    # test for 2pi functions
+                    if "mod2pi" == string($M.$f)
+                        goo = 4pi + $modifier
+                        @test NaN === $deriv
+                    end
+                end
+            elseif arity == 2
+                # TODO: Add test for types.
+                @test DiffRules.hasdiffrule(M, f, 2)
+                derivs = DiffRules.diffrule(M, f, :foo, :bar)
+                @eval begin
+                    foo, bar = rand(1:10), rand()
+                    dx, dy = $(derivs[1]), $(derivs[2])
+                    if !(isnan(dx))
+                        @test isapprox(dx, finitediff(z -> $M.$f(z, bar), float(foo)), rtol=0.05)
+                    end
+                    if !(isnan(dy))
+                        @test isapprox(dy, finitediff(z -> $M.$f(foo, z), bar), rtol=0.05)
+                    end
+                end
             end
         end
     end
