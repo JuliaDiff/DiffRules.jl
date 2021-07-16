b149ba5543bc6622390a1b4cce81f22fcd5c92dc
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-16 08:19:41 +0100

    depend on IrrationalConstants.jl

--- src/constants.jl
+++ src/constants.jl
@@ -1,6 +1,6 @@
 import Base: @irrational
+import IrrationalConstants: logtwo
 
-@irrational logtwo 0.6931471805599453094 log(big(2))
 @irrational logten 2.302585092994046 log(big(10))
 
 @irrational twoinvsqrtπ 1.1283791670955126 2 / sqrt(big(π))
